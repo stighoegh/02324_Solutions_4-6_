e71c9b0a335edab082955b1c21683b6ae97c7104
stighoegh <stighoegh@gmail.com>
2020-03-02 10:31:55 +0100

    vector opdateret

--- 4_Collections_Vector_interface/src/vector/Main.java
+++ 4_Collections_Vector_interface/src/vector/Main.java
@@ -1,25 +1,22 @@
 package vector;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        IVector[] vectors = new IVector[6]; // kan der s�ttes I foran den f�rste og den anden
-
-        //Tilf�j elementer til listen
+        IVector[] vectors = new IVector[6]; // kan der sættes I foran den første og den anden
+        //Tilføj elementer til listen
         vectors[0] = new Vector(1, 1);
         vectors[1] = new Vector(400, 278);
         vectors[2] = new Vector(3.0, 7);
         vectors[3] = new Vector(1, 2);
-        vectors[4] = vectors[0].add(vectors[2]); //Vi er n�dt til at caste til vector.Vector
-        vectors[5] = (Vector) vectors[0].times(4); //Vi er n�dt til at caste til vector.Vector
+        vectors[4] = vectors[0].add(vectors[2]);
+        vectors[5] = vectors[0].times(4); //Vi er nødt til at caste til vector.Vector
 
-        System.out.println("vectors f�r sortering: " + Arrays.toString(vectors));
+        System.out.println("vectors før sortering: " + Arrays.toString(vectors));
 
-        //Sort�r listen
+        //Sorter listen
         Arrays.sort(vectors);
         System.out.println("vectors efter sortering: " + Arrays.toString(vectors));
 
