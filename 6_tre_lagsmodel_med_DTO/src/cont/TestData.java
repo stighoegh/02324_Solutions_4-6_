9fb0ca0a7b16b6e725b0d4ce111ba96168987439
S174244 <38386533+S174244@users.noreply.github.com>
2020-04-01 01:15:47 +0200

    Added SQL option

--- 6_tre_lagsmodel_med_DTO/src/cont/TestData.java
+++ 6_tre_lagsmodel_med_DTO/src/cont/TestData.java
@@ -1,41 +1,46 @@
 package cont;
+
 import data.Data;
 import data.DataToDisk;
+import data.DataSQL;
 import data.IData;
 import dto.IngDTO;
 import java.util.List;
+
 public class TestData {
-    public static void main(String[] args) {
-        IData d;
-        boolean writeToDisk = false;
-        if (!writeToDisk)
-            d = new Data();
-        else d = new DataToDisk();
+	public static void main(String[] args) {
+		IData d;
+		boolean writeToDisk = false;
+		boolean writeToSQL = true;
+		if (writeToDisk) {
+			d = new DataToDisk();
+			System.out.println("Using Disk method");
+		} else if (writeToSQL) {
+			d = new DataSQL();
+			System.out.println("Using SQL method");
+		} else {
+			d = new Data();
+			System.out.println("Using Data method");
+		}
+		
+		for (IngDTO s : d.getAllIngredients()) {
+			System.out.println(s);
+		}
+		try {
+			d.createIngredient(14, "Ã¸l", 222);
+		} catch (IData.NummerFindesAllerede e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			d.createIngredient(new IngDTO(15, "vand", 300));
+		} catch (IData.NummerFindesAllerede e) {
+			System.out.println(e.getMessage());
+		}
 
-        for (IngDTO s : d.getAllIngredients()) {
-            System.out.println(s);
-        }
-        try{
-            d.createIngredient(14, "øl", 222);
-        }catch (IData.NummerFindesAllerede e){
-            System.out.println(e.getMessage());
-        }
-        try{
-            d.createIngredient(new IngDTO(15, "vand", 300));
-        }catch (IData.NummerFindesAllerede e){
-            System.out.println(e.getMessage());
-        }
-
-
-
-
-
-
-
-        for (IngDTO s : d.getAllIngredients()) {
-            System.out.println(s);
-        }
-    }
+		for (IngDTO s : d.getAllIngredients()) {
+			System.out.println(s);
+		}
+	}
 }
 
 //        List<IngDTO> list = d.getAllIngredients();
